e05869070bb15a278287c061b097c8e05dc4f259
ahdianmirza <ahdianmirza@gmail.com>
2023-11-25 17:19:33 +0700

    Multiple Components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import './App.css';
+import Home from './Home';
+import Navbar from './Navbar';
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  // const person = {
-  //   name: "ahdian",
-  //   age: 22
-  // };
-  const link = "http://www.google.com";
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-
-        {/* <p>{person}</p> */}
-
-        <p>{10}</p>
-        <p>{"Hello ninja's"}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.random() * 10}</p>
-
-        <a href={link}>Google Sites</a>
+        <Home />
       </div>
     </div>
   );
