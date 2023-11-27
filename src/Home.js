8a0353cc3fcc5efccc35353c1c00040da46460f3
ahdianmirza <ahdianmirza@gmail.com>
2023-11-27 14:47:55 +0700

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,18 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log("Hello ninjas", e);
+    };
+
+    const handleClickAgain = (name, e) => {
+      console.info(`Hello ${name}`, e.target);
+    };
+
     return (
-        <div className="home">
-            <h2>Homepage</h2>
-        </div>
+      <div className="home">
+        <h2>Homepage</h2>
+        <button onClick={handleClick}>Click me</button>
+        <button onClick={(e) => handleClickAgain("ahdian", e)}>Click me again</button>
+      </div>
     );
 }
 
