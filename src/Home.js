8359aef3178a33d9e0dae5fc632692a11f15a98f
ahdianmirza <ahdianmirza@gmail.com>
2023-11-27 15:15:27 +0700

    Outputting list

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,25 @@
 import { useState } from "react";
 
 const Home = () => {
-  // let name = "Ahdian";
-  const [name, setName] = useState("ahdian");
-  const [age, setAge] = useState(22);
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
-  const handleClick = () => {
-    setName("mirza");
-    setAge(30);
-  };
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{name} is {age} years old</p>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 }
